03a752393894b144a69d3548376d267428f37a80
brendaloya <l.loya.b@gmail.com>
2021-12-27 23:13:00 -0400

    deployment scripts working

--- scripts/01_DeployTellorFlexwithtestStakingToken.js
+++ scripts/01_DeployTellorFlexwithtestStakingToken.js
@@ -8,15 +8,15 @@ require("dotenv").config();
 const web3 = require('web3');
 
 //const dotenv = require('dotenv').config()
-//npx hardhat run scripts/01_DeployTellorFlex.js --network rinkeby
+//npx hardhat run scripts/01_DeployTellorFlexwithtestStakingToken.js --network rinkeby
 
 var stake_amt = web3.utils.toWei("10");
 var rep_lock = 43200; // 12 hours
-var stakerTokenAdd= '0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0'
+
 var governanceAddress = '0x20bEC8F31dea6C13A016DC7fCBdF74f61DC8Ec2c'
 
 
-async function deployTellorFlex(_network, _pk, _nodeURL, stakerToken, govAdd, stakeAmount, reporterLock) {
+async function deployTellorFlex(_network, _pk, _nodeURL,  govAdd, stakeAmount, reporterLock) {
     console.log("deploy tellorFlex")
     await run("compile")
 
@@ -27,6 +27,38 @@ async function deployTellorFlex(_network, _pk, _nodeURL, stakerToken, govAdd, st
     var provider = new ethers.providers.JsonRpcProvider(_nodeURL)
     let wallet = new ethers.Wallet(privateKey, provider)
 
+    //////////Deploy test token
+    console.log("Starting deployment for staking token contract...")
+    const stakingToken = await ethers.getContractFactory("contracts/testing/StakingToken.sol:StakingToken", wallet)
+    const sTokenwithsigner = await stakingToken.connect(wallet)
+    const st = await sTokenwithsigner.deploy()
+    await st.deployed();
+
+    if (net == "mainnet") {
+        console.log("stakingToken contract deployed to:", "https://etherscan.io/address/" + st.address);
+        console.log("    stakingToken transaction hash:", "https://etherscan.io/tx/" + st.deployTransaction.hash);
+    } else if (net == "rinkeby") {
+        console.log("stakingToken contract deployed to:", "https://rinkeby.etherscan.io/address/" + st.address);
+        console.log("    stakingToken transaction hash:", "https://rinkeby.etherscan.io/tx/" + st.deployTransaction.hash);
+    } else {
+        console.log("Please add network explorer details")
+    }
+
+    // Wait for few confirmed transactions.
+    // Otherwise the etherscan api doesn't find the deployed contract.
+    console.log('waiting for stakingToken tx confirmation...');
+    await st.deployTransaction.wait(7)
+
+    console.log('submitting stakingToken contract for verification...');
+
+    await run("verify:verify",
+        {
+            address: st.address
+             
+        },
+    )
+
+    console.log("TellorFlex contract verified")
 
     /////////// Deploy Tellor flex
     console.log("deploy tellor flex")
@@ -36,9 +68,8 @@ async function deployTellorFlex(_network, _pk, _nodeURL, stakerToken, govAdd, st
     console.log("Starting deployment for TellorFlex contract...")
     const tellorF = await ethers.getContractFactory("contracts/TellorFlex.sol:TellorFlex", wallet)
     const tellorFwithsigner = await tellorF.connect(wallet)
-    const tellor = await tellorFwithsigner.deploy(stakerToken, govAdd, stakeAmount, reporterLock)
+    const tellor = await tellorFwithsigner.deploy(st.address, govAdd, stakeAmount, reporterLock)
     await tellor.deployed();
-
 
     if (net == "mainnet") {
         console.log("TellorFlex contract deployed to:", "https://etherscan.io/address/" + tellor.address);
@@ -60,20 +91,16 @@ async function deployTellorFlex(_network, _pk, _nodeURL, stakerToken, govAdd, st
     await run("verify:verify",
         {
             address: tellor.address,
-            constructorArguments: [stakerToken, govAdd, stakeAmount, reporterLock] 
+            constructorArguments: [st.address, govAdd, stakeAmount, reporterLock] 
         },
     )
 
     console.log("TellorFlex contract verified")
 
-  
-
-
-
 }
 
 
-deployTellorFlex("rinkeby", process.env.TESTNET_PK, process.env.NODE_URL_RINKEBY,stakerTokenAdd,governanceAddress,stake_amt,rep_lock)
+deployTellorFlex("rinkeby", process.env.TESTNET_PK, process.env.NODE_URL_RINKEBY,governanceAddress,stake_amt,rep_lock)
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
